2c2647f4781343b05d53514fc672093ed6c09673
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2024-10-17 20:22:12 -0300

    refact(API-109): removing unused fields and renaming the tables

--- DDLsIaScore/iaScoreResultData.sql
+++ DDLsIaScore/iaScoreResultData.sql
@@ -4,6 +4,5 @@ CREATE TABLE public.ai_score_results (
     final_score INTEGER NOT NULL,                         -- Final calculated score
     input_variables JSONB NOT NULL,                       -- Input variables used in the score calculation (stored as JSONB)
     endorser_name VARCHAR(255) NOT NULL,                  -- Name of the endorser
-    record_status VARCHAR(50) DEFAULT 'active',           -- Status of the record (e.g., 'active', 'cancelled', 'finished')
     created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp for when the record was created
 );
