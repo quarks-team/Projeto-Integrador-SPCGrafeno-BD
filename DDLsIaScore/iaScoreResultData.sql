38d67ecaad736b1affa3795328b890bb5a3dd31a
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2024-10-18 22:22:16 -0300

    docs(API-48): added the data dictionary for the AI score (#5)
    * docs(API-48): remaking the data dictionary so it is according to the new tables
    
    * removing useless column entry_date

--- DDLsIaScore/iaScoreResultData.sql
+++ DDLsIaScore/iaScoreResultData.sql
@@ -1,6 +1,5 @@
 CREATE TABLE public.ai_score_results (
     result_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- Unique identifier for each result
-    entry_date DATE NOT NULL,                             -- Date the data was processed
     final_score INTEGER NOT NULL,                         -- Final calculated score
     input_variables JSONB NOT NULL,                       -- Input variables used in the score calculation (stored as JSONB)
     endorser_name VARCHAR(255) NOT NULL,                  -- Name of the endorser
